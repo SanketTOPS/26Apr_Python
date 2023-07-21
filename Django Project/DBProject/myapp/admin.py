b77d3a00e578e8fa2f7a2ba173254084110f48ce
SanketTops <sanket.tops@gmail.com>
2023-07-21 09:59:50 +0530

    DBApp

---
+++ Django Project/DBProject/myapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import userinfo
+
+# Register your models here.
+
+admin.site.register(userinfo)
---
+++ Django Project/DBProject/myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class userinfo(models.Model):
+    firstname=models.CharField(max_length=20)
+    lastname=models.CharField(max_length=20)
+    email=models.EmailField()
+    dob=models.DateField()
+    mobile=models.BigIntegerField()
+    address=models.TextField()
