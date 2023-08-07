b09702d7ff0ebb587e0253c6ef9dd5ce9e93eed2
SanketTops <sanket.tops@gmail.com>
2023-08-07 10:00:04 +0530

    Signup

--- Django Project/AuthProject/myapp/models.py
+++ Django Project/AuthProject/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class userSignup(models.Model):
+    created=models.DateTimeField(auto_now_add=True)
+    firstname=models.CharField(max_length=20)
+    lastname=models.CharField(max_length=20)
+    username=models.CharField(max_length=20)
+    password=models.CharField(max_length=12)
+    city=models.CharField(max_length=20)
+    state=models.CharField(max_length=20)
+    mobile=models.BigIntegerField()
+
--- Django Project/AuthProject/myapp/views.py
+++ Django Project/AuthProject/myapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import signupForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -6,4 +8,14 @@ def index(request):
     return render(request,'index.html')
 
 def signup(request):
+    if request.method=='POST':
+        newuser=signupForm(request.POST)
+        if newuser.is_valid():
+            newuser.save()
+            print("Signup Successfully!")
+            messages.success(request,"Signup Successfully!")
+            #return redirect('/')
+        else:
+            print(newuser.errors)
+            messages.error(request,"Error! Something went wrong...")
     return render(request,'signup.html')
