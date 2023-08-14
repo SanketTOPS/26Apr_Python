4a1e57280223ea491986052ab845b589e3d52ea4
SanketTops <sanket.tops@gmail.com>
2023-08-14 09:56:59 +0530

    Project Start - Index Page

---
+++ Django Project/MyNotes/notesapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from notesapp import views
+
+urlpatterns = [
+    path('',views.index),
+    path('notes/',views.notes),
+    path('profile/',views.profile),
+    path('about/',views.about),
+    path('contact/',views.contact),
+]
